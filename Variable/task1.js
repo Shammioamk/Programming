ea05cea338d3f98b8d67b0ece70b9bbb836c1816
Shamima Shammi <t2shsh00@students.oamk.fi>
2022-11-20 00:59:33 +0200

    Today I solve some Variable problem

--- Variable/task1.js
+++ Variable/task1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 process.stdout.write("This progam is a computer game. Please type in a number :");
 process.stdin.on ( "data", function(inputFromUser)
@@ -9,16 +8,4 @@ console.log("\nMy number is:"+(Number(inputFromUser)+1));
 console.log("\nSorry you lost, I won. The game is over");
 process.exit() ;
 
-=======
-
-process.stdout.write("This progam is a computer game. Please type in a number :");
-process.stdin.on ( "data", function(inputFromUser)
-{
-
-console.log("\nYou typed in number:"+(Number(inputFromUser)));
-console.log("\nMy number is:"+(Number(inputFromUser)+1));
-console.log("\nSorry you lost, I won. The game is over");
-process.exit() ;
-
->>>>>>> 4caa5405060a551ded6499c75d45734fa40b1c72
 } ) ;
--- Variable/task4.js
+++ Variable/task4.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 process.stdout.write("Please enter a number :");
 process.stdin.on ( "data", function(inputFromUser)
 {
@@ -29,35 +28,4 @@ process.stdout.write("\n"+(Number(inputFromUser)+1)+"--"+" = "+((Number(inputFro
 process.exit() ;
 
 } ) ;
-=======
-process.stdout.write("Please enter a number :");
-process.stdin.on ( "data", function(inputFromUser)
-{
-// The statements below will be executed automatically after
-// the user has typed in something.
 
-let secretnumber = inputFromUser;
-process.stdout.write(Number(inputFromUser)+ " + " + Number(secretnumber)); 
-process.stdout.write(" = "+(Number(secretnumber)+Number(secretnumber)));
-
-process.stdout.write("\n"+Number(inputFromUser)+ " - " + Number(secretnumber)); 
-process.stdout.write(" = "+(Number(secretnumber)-Number(secretnumber)));
-
-process.stdout.write("\n"+Number(inputFromUser)+ " * " + Number(secretnumber)); 
-process.stdout.write(" = "+(Number(secretnumber)*Number(secretnumber)));
-
-process.stdout.write("\n"+Number(inputFromUser)+ " / " + Number(secretnumber)); 
-process.stdout.write(" = "+(Number(secretnumber)/Number(secretnumber)));
-
-process.stdout.write("\n"+Number(inputFromUser)+ " % " + Number(secretnumber)); 
-process.stdout.write(" = "+(Number(secretnumber)%Number(secretnumber)));
-
-process.stdout.write("\n"+Number(inputFromUser)+"++");
-process.stdout.write(" = "+(Number(inputFromUser)+1));
-
-process.stdout.write("\n"+(Number(inputFromUser)+1)+"--"+" = "+((Number(inputFromUser)+1)-1));
-
-process.exit() ;
-
-} ) ;
->>>>>>> 4caa5405060a551ded6499c75d45734fa40b1c72
